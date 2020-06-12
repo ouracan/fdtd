7aa5c3f8ff43adbf815bd55b786133266f56d0e0
Zheng Zu <zhengzu@pm.me>
2020-06-12 00:06:27 -0700

    Initial Commit

---
+++ Improved_1D_FDTD_v05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Improved_1D_FDTD_v05
+//
+//  Created by Zheng Zu on 12.06.20.
+//  Copyright © 2020 Zheng Zu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
